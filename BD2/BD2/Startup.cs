652cf7f9e367cb3aa42be2e73f0f410efaf90e1f
Pawel <leszczak.pm@gmail.com>
2019-12-07 17:17:09 +0100

    Move connection string to secrets

--- BD2/BD2/Startup.cs
+++ BD2/BD2/Startup.cs
@@ -29,15 +29,23 @@ namespace BD2
         {
             services.AddControllers();
             services.AddDbContext<DataContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DataContext")));
+                options.UseSqlServer(Configuration["ConnectionString"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json",
+                             optional: false,
+                             reloadOnChange: true)
+                .AddEnvironmentVariables();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                builder.AddUserSecrets<Startup>();
             }
 
             app.UseHttpsRedirection();
