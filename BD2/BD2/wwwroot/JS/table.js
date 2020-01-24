d0cad7c5b3405c27676f6a9f554f305c5db9d10c
SmutnyPan <wojtek.sadlik38@gmail.com>
2020-01-24 12:58:23 +0100

    separate generateTable and generateTable with links in tables

--- BD2/BD2/wwwroot/JS/table.js
+++ BD2/BD2/wwwroot/JS/table.js
@@ -1,4 +1,4 @@
-function generateMainTable(dataJSON, entityName) {
+function generateTableWithLinks(dataJSON, entityName) {
     let htmltxt = '<tr>';
 
     if (Array.isArray(dataJSON) && dataJSON.length) {
@@ -18,6 +18,34 @@ function generateMainTable(dataJSON, entityName) {
             }
             
             htmltxt += `<th><a href="${entityName}.html?id=${object['id']}">more...</a></th>`;
+            
+            htmltxt += '</tr>';
+        });    
+    } else {
+        htmltxt = '<th>empty</th></tr>';
+    }
+
+    return htmltxt
+}
+
+function generateTable(dataJSON) {
+    let htmltxt = '<tr>';
+
+    if (Array.isArray(dataJSON) && dataJSON.length) {
+        for (atr in dataJSON[0]) {
+            if (!isForeignId(atr))
+                htmltxt += '<th>' + atr + '</th>';
+        }
+        
+        htmltxt += '<th>more</th></tr>';
+    
+        dataJSON.forEach(object => {
+            htmltxt += '<tr>';
+    
+            for (atr in object) {
+                if (!isForeignId(atr))
+                    htmltxt += '<th>' + object[atr] + '</th>';
+            }
             
             htmltxt += '</tr>';
         });    
