b81fb8336ecfdeadbf5530322ef2e47c79aa8283
SmutnyPan <wojtek.sadlik38@gmail.com>
2020-01-26 11:11:38 +0100

    refactoring

---
+++ BD2/BD2/wwwroot/JS/addingData.js
@@ -0,0 +1,56 @@
+dataForms = {
+    "users":  {
+        "email": "",
+        "name": "",
+        "surname": "",
+        "authorizationId": -1,
+        "outpostId": -1
+    },
+    "atributes": {
+        "name": "",
+        "itemIds": []
+    },
+    "authorizations": {
+        "name": "",
+        "canEdit": false,
+        "isGlobal": true
+    },
+    "comments": {
+        "title": "",
+        "text": null,
+        "photoId": -1,
+        "itemId": -1,
+        "userId": -1
+    },
+    "globalAtributes": {
+        "name": "",
+        "content": "",
+        "itemIds": []
+    },
+    "groups": {
+        "name": "",
+        "description": "",
+        "itemIds": []
+    },
+    "items": {
+        "name": "",
+        "description": "",
+        "photoId": -1,
+        "atributeIds": [],
+        "globalAtributeIds": [],
+        "groupIds": []
+    },
+    "localItems": {
+
+    },
+    "outposts": {
+        "name": "",
+        "location": ""
+    },
+    "photos": {
+        "comment": ""
+    },
+    "values": {
+
+    }
+}
